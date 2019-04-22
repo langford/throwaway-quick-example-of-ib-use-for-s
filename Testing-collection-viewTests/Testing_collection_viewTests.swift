1e6f6c01fed30b2b55ee129a88825c77ddc23be7
Michael Langford <michael.langford@rowdylabs.com>
2019-04-22 16:44:56 -0400

    Initial Commit

---
+++ Testing-collection-viewTests/Testing_collection_viewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Testing_collection_viewTests.swift
+//  Testing-collection-viewTests
+//
+//  Created by Michael Langford on 4/22/19.
+//  Copyright © 2019 Rowdy Labs LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Testing_collection_view
+
+class Testing_collection_viewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
